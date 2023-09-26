748466c919d151899810e428f7d436849b6f9557
Giovane Forlenza <giovane.franciscoforl@gmail.com>
2023-09-26 18:58:15 -0300

    managed to get the API to work but not fully

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,44 +1,117 @@
 import React, { useCallback, useEffect, useState } from "react";
-import axios from "axios";
 
 let resp = "";
 let data;
 const API_KEY = "e37ac004829824b7fe9c4723fed7bd07";
-let weatherIconURL;
+let weatherIconURL, cityCoordFromNameLink;
+
+/*
+
+1 LOAD THE PAGE - default NY
+2 SEARCH FOR CITY
+3 PARSE COORDS
+4 GET CITY INFO
+5 DISPLAY CITY INFO
+
+1 LOAD PAGE - USE EFFECT
+2 Search bar - ENTER
+3 Get city coords
+
+*/
 
 function Home() {
-  const [weatherData, setWeatherData] = useState([]);
+  const [weatherData, setWeatherData] = useState({
+    name: "",
+    temp: "",
+    temp_max: "",
+    temp_min: "",
+  });
+
+  const [cityLat, setCityLat] = useState("40.7127281");
+  const [cityLon, setCityLon] = useState("-74.0060152");
+  const [searchTerm, setSearchTerm] = useState("");
+
+  function updateWeatherData() {
+    weatherIconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+
+    setWeatherData({
+      name: data.name,
+      temp: data.main.temp,
+      temp_max: data.main.temp_max,
+      temp_min: data.main.temp_min,
+    });
+  }
+
   const getWeather = useCallback(async () => {
     try {
       resp = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=Atibaia&appid=${API_KEY}&units=metric`
+        `https://api.openweathermap.org/data/2.5/weather?lat=${cityLat}&lon=${cityLon}&appid=${API_KEY}&units=metric`
       );
       data = await resp.json();
-
-      console.log("info", data);
-      weatherIconURL = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-      setWeatherData([
-        data.name,
-        data.main.temp,
-        data.main.temp_max,
-        data.main.temp_min,
-      ]);
+      updateWeatherData();
     } catch (error) {
       console.log(error);
     }
   });
 
+  const updateSearchTerm = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  function submitForm(e) {
+    e.preventDefault();
+    getCityCoords();
+  }
+
+  const getCityCoords = useCallback(async () => {
+    getCityCoordsLink();
+    try {
+      const a = await fetch(`${cityCoordFromNameLink}`);
+      const b = await a.json();
+      const cityLat = b[0].lat,
+        cityLon = b[0].lon;
+      setCityLat(cityLat);
+      setCityLon(cityLon);
+      getWeather();
+    } catch (error) {
+      console.log(error);
+    }
+
+    function getCityCoordsLink() {
+      let rep = "%20";
+      let ogSearch = searchTerm;
+      for (let i = 0; i < ogSearch.length; i++) {
+        if (ogSearch[i] == " ") ogSearch = ogSearch.replace(ogSearch[i], rep);
+      }
+      cityCoordFromNameLink =
+        "http://api.openweathermap.org/geo/1.0/direct?q=" +
+        ogSearch +
+        "&appid=" +
+        API_KEY;
+    }
+  });
+
   useEffect(() => {
     getWeather();
-  }, []);
+  }, [cityLat]);
+
   return (
     <div>
-      city: {weatherData[0]} <br />
-      temp {weatherData[1]}
+      <form action="" onSubmit={submitForm}>
+        <input
+          type="text"
+          name=""
+          id=""
+          value={searchTerm}
+          onChange={updateSearchTerm}
+        />
+      </form>
+      city: {weatherData.name} <br />
+      temp {weatherData.temp}
       <br />
-      max temp {weatherData[2]}
+      max temp {weatherData.temp_max}
       <br />
-      min temp {weatherData[3]} <br />
+      min temp {weatherData.temp_min} <br />
       <img src={weatherIconURL} alt="" />
     </div>
   );
